96beeb1582717c4b54ec1ef50fbe43dee300b4e6
Godfrey Cortez <godfreycortez@outlook.com>
2021-03-29 20:54:27 -0400

    allow public key retrieval

--- client/src/main/java/app/Driver.java
+++ client/src/main/java/app/Driver.java
@@ -17,7 +17,7 @@ public class Driver {
         String username = console.readLine("Enter username: ");
         char[] password = console.readPassword("Enter password: ");
 
-        String connectString = "jdbc:mysql://marmoset04.shoshin.uwaterloo.ca:3306/project_37?useSSL=false";
+        String connectString = "jdbc:mysql://marmoset04.shoshin.uwaterloo.ca:3306/project_37?useSSL=false&allowPublicKeyRetrieval=true";
 
         try (Connection con = DriverManager.getConnection(connectString, username, password.toString())) {
             String query = "SELECT VERSION()";

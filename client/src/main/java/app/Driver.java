ee9c6dd80dcc5067ac8e6f7786a598bf5e44bdb3
Godfrey Cortez <godfreycortez@outlook.com>
2021-03-31 23:18:46 -0400

    Add Terminal

--- client/src/main/java/app/Driver.java
+++ client/src/main/java/app/Driver.java
@@ -7,6 +7,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+
 public class Driver {
     public static void main(String[] args) {
         Console console = System.console();

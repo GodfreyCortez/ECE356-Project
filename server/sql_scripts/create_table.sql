afc32ad13049b7112a3f582c231f08079539921d
James Palathunkal <jpalathu@uwaterloo.ca>
2021-04-01 22:16:14 -0400

    fixed most of the warnings when loading data

--- server/sql_scripts/create_table.sql
+++ server/sql_scripts/create_table.sql
@@ -36,21 +36,21 @@ create table Stock (
 select '----------------------------------------------------------------' as '';
 select 'Indicator' as '';
 create table Indicator (
-    year int not null,
+    year decimal(4) not null,
      symbol char(5) not null,
-     eps decimal(11, 4),
-     revenue int,
-     costOfRevenue int,
-     grossProfit int,
-     peRatio decimal(8, 4),
+     eps decimal(13, 4),
+     revenue bigint,
+     costOfRevenue bigint,
+     grossProfit bigint,
+     peRatio decimal(10, 4),
      debtRatio decimal(8, 4),
-    dividendYield decimal(9, 6),
-    dividendPayoutRatio decimal(9, 6),
+    dividendYield decimal(12, 9),
+    dividendPayoutRatio decimal(12, 9),
     pbValueRatio decimal(13, 4),
-    pegRatio decimal(8, 4),
-    epsGrowth decimal(7, 4),
+    pegRatio decimal(15, 9),
+    epsGrowth decimal(8, 4),
     sector char(25) not null,
-    priceVar decimal(12, 7) not null,
+    priceVar decimal(16, 9) not null,
     primary key (year, symbol)
 );
 
@@ -59,40 +59,95 @@ load data infile '/var/lib/mysql-files/18-Stocks/Project37/2014_Financial_Data_C
      fields terminated by ','
      lines terminated by '\n'
      ignore 1 lines
-     (symbol, revenue, costOfRevenue, grossProfit, eps, pbValueRatio, peRatio, pegRatio, dividendYield, debtRatio, dividendPayoutRatio, epsGrowth, sector, priceVar)
-     set year = 2014;
+     (symbol, @revenue, @costOfRevenue, @grossProfit, @eps, @pbValueRatio, @peRatio, @pegRatio, @dividendYield, @debtRatio, @dividendPayoutRatio, @epsGrowth, sector, priceVar)
+     set  year = 2014,
+          revenue = nullif(@revenue, ''),
+          costOfRevenue = nullif(@costOfRevenue, ''),
+          grossProfit = nullif(@grossProfit, ''),
+          eps = nullif(@eps, ''),
+          pbValueRatio = nullif(@pbValueRatio,''),
+          peRatio = nullif(@peRatio,''),
+          pegRatio = nullif(@pegRatio,''),
+          dividendYield = nullif(@dividendYield,''),
+          debtRatio = nullif(@debtRatio,''),
+          dividendPayoutRatio = nullif(@dividendPayoutRatio,''),
+          epsGrowth = nullif(@epsGrowth,'');
 
 -- load the 2015 Indicator data into Indicator table
 load data infile '/var/lib/mysql-files/18-Stocks/Project37/2015_Financial_Data_Clean.csv' ignore into table Indicator
      fields terminated by ','
      lines terminated by '\n'
      ignore 1 lines
-     (symbol, revenue, costOfRevenue, grossProfit, eps, pbValueRatio, peRatio, pegRatio, dividendYield, debtRatio, dividendPayoutRatio, epsGrowth, sector, priceVar)
-     set year = 2015;
+     (symbol, @revenue, @costOfRevenue, @grossProfit, @eps, @pbValueRatio, @peRatio, @pegRatio, @dividendYield, @debtRatio, @dividendPayoutRatio, @epsGrowth, sector, priceVar)
+     set  year = 2015,
+          revenue = nullif(@revenue, ''),
+          costOfRevenue = nullif(@costOfRevenue, ''),
+          grossProfit = nullif(@grossProfit, ''),
+          eps = nullif(@eps, ''),
+          pbValueRatio = nullif(@pbValueRatio,''),
+          peRatio = nullif(@peRatio,''),
+          pegRatio = nullif(@pegRatio,''),
+          dividendYield = nullif(@dividendYield,''),
+          debtRatio = nullif(@debtRatio,''),
+          dividendPayoutRatio = nullif(@dividendPayoutRatio,''),
+          epsGrowth = nullif(@epsGrowth,'');
 
 -- load the 2016 Indicator data into Indicator table
 load data infile '/var/lib/mysql-files/18-Stocks/Project37/2016_Financial_Data_Clean.csv' ignore into table Indicator
      fields terminated by ','
      lines terminated by '\n'
      ignore 1 lines
-     (symbol, revenue, costOfRevenue, grossProfit, eps, pbValueRatio, peRatio, pegRatio, dividendYield, debtRatio, dividendPayoutRatio, epsGrowth, sector, priceVar)
-     set year = 2016;
+     (symbol, @revenue, @costOfRevenue, @grossProfit, @eps, @pbValueRatio, @peRatio, @pegRatio, @dividendYield, @debtRatio, @dividendPayoutRatio, @epsGrowth, sector, priceVar)
+     set  year = 2016,
+          revenue = nullif(@revenue, ''),
+          costOfRevenue = nullif(@costOfRevenue, ''),
+          grossProfit = nullif(@grossProfit, ''),
+          eps = nullif(@eps, ''),
+          pbValueRatio = nullif(@pbValueRatio,''),
+          peRatio = nullif(@peRatio,''),
+          pegRatio = nullif(@pegRatio,''),
+          dividendYield = nullif(@dividendYield,''),
+          debtRatio = nullif(@debtRatio,''),
+          dividendPayoutRatio = nullif(@dividendPayoutRatio,''),
+          epsGrowth = nullif(@epsGrowth,'');
 
 -- load the 2017 Indicator data into Indicator table
 load data infile '/var/lib/mysql-files/18-Stocks/Project37/2017_Financial_Data_Clean.csv' ignore into table Indicator
      fields terminated by ','
      lines terminated by '\n'
      ignore 1 lines
-     (symbol, revenue, costOfRevenue, grossProfit, eps, pbValueRatio, peRatio, pegRatio, dividendYield, debtRatio, dividendPayoutRatio, epsGrowth, sector, priceVar)
-     set year = 2017;
+     (symbol, @revenue, @costOfRevenue, @grossProfit, @eps, @pbValueRatio, @peRatio, @pegRatio, @dividendYield, @debtRatio, @dividendPayoutRatio, @epsGrowth, sector, priceVar)
+     set  year = 2017,
+          revenue = nullif(@revenue, ''),
+          costOfRevenue = nullif(@costOfRevenue, ''),
+          grossProfit = nullif(@grossProfit, ''),
+          eps = nullif(@eps, ''),
+          pbValueRatio = nullif(@pbValueRatio,''),
+          peRatio = nullif(@peRatio,''),
+          pegRatio = nullif(@pegRatio,''),
+          dividendYield = nullif(@dividendYield,''),
+          debtRatio = nullif(@debtRatio,''),
+          dividendPayoutRatio = nullif(@dividendPayoutRatio,''),
+          epsGrowth = nullif(@epsGrowth,'');
 
 -- load the 2018 Indicator data into Indicator table
 load data infile '/var/lib/mysql-files/18-Stocks/Project37/2018_Financial_Data_Clean.csv' ignore into table Indicator
      fields terminated by ','
      lines terminated by '\n'
      ignore 1 lines
-     (symbol, revenue, costOfRevenue, grossProfit, eps, pbValueRatio, peRatio, pegRatio, dividendYield, debtRatio, dividendPayoutRatio, epsGrowth, sector, priceVar)
-     set year = 2018;
+     (symbol, @revenue, @costOfRevenue, @grossProfit, @eps, @pbValueRatio, @peRatio, @pegRatio, @dividendYield, @debtRatio, @dividendPayoutRatio, @epsGrowth, sector, priceVar)
+     set  year = 2018,
+          revenue = nullif(@revenue, ''),
+          costOfRevenue = nullif(@costOfRevenue, ''),
+          grossProfit = nullif(@grossProfit, ''),
+          eps = nullif(@eps, ''),
+          pbValueRatio = nullif(@pbValueRatio,''),
+          peRatio = nullif(@peRatio,''),
+          pegRatio = nullif(@pegRatio,''),
+          dividendYield = nullif(@dividendYield,''),
+          debtRatio = nullif(@debtRatio,''),
+          dividendPayoutRatio = nullif(@dividendPayoutRatio,''),
+          epsGrowth = nullif(@epsGrowth,'');
 
 -- insert into Sector table
 insert into Sector select distinct sector from Indicator;
@@ -108,7 +163,7 @@ select '----------------------------------------------------------------' as '';
 select 'News' as '';
 create table News (
 	date date not null,
-    headline varchar(255) not null,
+    headline varchar(500) not null,
     symbol char(5) not null,
     publisher varchar(255) not null,
     url varchar(255) not null,
@@ -121,7 +176,8 @@ load data infile '/var/lib/mysql-files/18-Stocks/raw_analyst_ratings.csv' ignore
      optionally enclosed by '"' escaped by '\b'
      lines terminated by '\n'
      ignore 1 lines
-     (@ignore, headline, url, publisher, date, symbol);
+     (@ignore, headline, url, publisher, @date, symbol)
+     set date = date_format(@date, '%Y-%m-%d');
 
 -- load News table with Benzinga partner data
 load data infile '/var/lib/mysql-files/18-Stocks/raw_partner_headlines.csv' ignore into table News
@@ -129,10 +185,11 @@ load data infile '/var/lib/mysql-files/18-Stocks/raw_partner_headlines.csv' igno
      optionally enclosed by '"' escaped by '\b'
      lines terminated by '\n'
      ignore 1 lines
-     (@ignore, headline, url, publisher, date, symbol);
+     (@ignore, headline, url, publisher, @date, symbol)
+     set date = date_format(@date, '%Y-%m-%d');
 
 -- insert into Stock table if stock doesn't exist already
-insert into Stock select distinct t1.symbol, NULL from News t1 where not exists ( select symbol from Stock t2 where t2.symbol = t1.symbol );
+insert into Stock select distinct t1.symbol, NULL from News t1 where not exists ( select distinct symbol from Stock t2 where t2.symbol = t1.symbol );
 -- add the foreign key (need to do after making sure Stock table has the stock)
 alter table News add constraint fk_News_Stock foreign key (symbol) references Stock(symbol);
 
@@ -147,12 +204,12 @@ select 'History' as '';
 create table History (
 	date date not null,
 	symbol char(5) not null,
-	open decimal(21, 16) not null,
-	high decimal(21, 16) not null,
-	low decimal(21, 16) not null,
-	close decimal(21, 16) not null,
-	adjClose decimal(21, 16) not null,
-	volume decimal(21, 16) not null,
+	open decimal(26, 18) not null,
+	high decimal(26, 18) not null,
+	low decimal(26, 18) not null,
+	close decimal(26, 18) not null,
+	adjClose decimal(26, 18) not null,
+	volume int unsigned not null,
     primary key (date, symbol)
 );
 
@@ -164,7 +221,7 @@ load data infile '/var/lib/mysql-files/18-Stocks/fh_5yrs.csv' ignore into table 
      (date, volume, open, high, low, close, adjClose, symbol);
 
 -- insert into Stock table
-insert into Stock select distinct t1.symbol, NULL from History t1 where not exists ( select symbol from Stock t2 where t2.symbol = t1.symbol );
+insert into Stock select distinct t1.symbol, NULL from History t1 where not exists ( select distinct symbol from Stock t2 where t2.symbol = t1.symbol );
 -- add the foreign key (need to do after making sure Stock table has the stock)
 alter table History add constraint fk_History_Stock foreign key (symbol) references Stock(symbol);
 

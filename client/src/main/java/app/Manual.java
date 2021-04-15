314d59c70cc85dee9b0ad1b727ffc1cfa15c8ec8
James Palathunkal <jpalathu@uwaterloo.ca>
2021-04-14 22:24:41 -0400

    added options to manual

--- client/src/main/java/app/Manual.java
+++ client/src/main/java/app/Manual.java
@@ -21,6 +21,8 @@ public class Manual {
     private static String indicator() {
         return  header("Indicator:") +
                 command("indicator [-s <stock>][-y <year>][-sec <sector>][-buy][-notBuy]") +
+                option("-buy will retrieve a list of stocks that you should buy at the beginning of a given year and sell at the end of the year, specify a year") +
+                option("-notBuy will retrieve a list of stocks that will decrease in value and you should not buy in a given year, specify a year") +
                 command("add indicator") +
                 command("delete indicator [-s <stock>][-y <year>]");
     }
@@ -31,9 +33,11 @@ public class Manual {
     }
     private static String history() {
         return  header("History:") +
-                command("info <-s <stock>> [-d <date>, -r <start-date> <end-date>][-52][-g]") +
+                command("info <-s <stock>> [-d <date>, -r <start-date> <end-date>][-52][-g][-exp][-cheap]") +
                 option("-g option will give general information about a stock (avg volume in past year, 52 week high and low, latest price info available)") +
                 option("-52 allows you to retrieve the 52 week high and low of the most recent date, specify an end date to get the 52 week range for that") +
+                option("-exp will retrieve the top 10 most expensive stocks by sector in a given year, specify a year and sector") +
+                option("-cheap will to retrieve the top 10 cheapest stocks by sector in a given year, specify a year and sector") +
                 command("add history") +
                 command("delete history <-s <stock>>[-d]");
     }
